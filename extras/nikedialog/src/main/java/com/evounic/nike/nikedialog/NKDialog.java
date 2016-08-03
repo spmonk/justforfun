19572c37d4a81ff16f91cc256314896d06ef7fa5
yaocl <yaochenglindlut@gmail.com>
2016-08-03 21:29:59 +0800

    add title

--- extras/nikedialog/src/main/java/com/evounic/nike/nikedialog/NKDialog.java
+++ extras/nikedialog/src/main/java/com/evounic/nike/nikedialog/NKDialog.java
@@ -1,14 +1,25 @@
 package com.evounic.nike.nikedialog;
 
 import android.content.Context;
+import android.support.annotation.UiThread;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 /**
  * Author:yaocl
  * Created on 2016/8/3.
+ * 自定义Dialog使用了Builder构造者模式，它的基本代码为
+ *
  */
 public class NKDialog extends NKDialogBase{
 
     protected final Builder mBuilder;
+
+    //title
+    protected ImageView titleIcon;
+    protected TextView title;
+    protected View titleView;
 
     //protected 不让外部直接调用进行初始化，但允许子类继承,通过Builder来调用生成相应实例
     protected NKDialog(Builder builder) {
@@ -16,11 +27,53 @@ public class NKDialog extends NKDialogBase{
         mBuilder = builder;
     }
 
+    public Builder getBuilder() {
+        return mBuilder;
+    }
+
+    @UiThread
+    public void show(){
+        // TODO: 2016/8/3 add show dialog exception
+            super.show();
+    }
+
     public static class Builder {
         protected final Context mContext;
+
+        private CharSequence title;
 
         public Builder(Context context) {
             mContext = context;
         }
+
+        public Builder title(CharSequence title) {
+            this.title = title;
+            return this;
+        }
+
+        public Builder title(int resId){
+            return title(mContext.getText(resId));
+        }
+
+
+        /**
+         * 创建Dialog
+         * @return
+         */
+        public NKDialog create(){
+            return new NKDialog(this);
+        }
+
+        /**
+         * 新创建一个Dialog并显示
+         * @return
+         */
+        public NKDialog show(){
+            NKDialog dialog = new NKDialog(this);
+            dialog.show();
+            return dialog;
+        }
+
+
     }
 }
--- extras/nikedialog/src/main/java/com/evounic/nike/nikedialog/NKDialogBase.java
+++ extras/nikedialog/src/main/java/com/evounic/nike/nikedialog/NKDialogBase.java
@@ -2,14 +2,23 @@ package com.evounic.nike.nikedialog;
 
 import android.app.Dialog;
 import android.content.Context;
+import android.view.View;
 
 /**
  * Author:yaocl
  * Created on 2016/8/3.
  */
 public class NKDialogBase extends Dialog{
+
+    protected View rootView;
+
     public NKDialogBase(Context context) {
         this(context,0);
+    }
+
+    @Override
+    public View findViewById(int id){
+        return rootView.findViewById(id);
     }
 
     public NKDialogBase(Context context, int themeResId) {
