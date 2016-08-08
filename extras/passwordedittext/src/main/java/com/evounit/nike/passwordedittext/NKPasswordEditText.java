dc9851a53d50b25958259b105ca21145693cac1b
yaocl <yaochenglindlut@gmail.com>
2016-08-08 21:08:46 +0800

    [add] passwordText

--- extras/passwordedittext/src/main/java/com/evounit/nike/passwordedittext/NKPasswordEditText.java
+++ extras/passwordedittext/src/main/java/com/evounit/nike/passwordedittext/NKPasswordEditText.java
@@ -2,9 +2,9 @@ package com.evounit.nike.passwordedittext;
 
 import android.content.Context;
 import android.content.res.TypedArray;
+import android.graphics.Canvas;
 import android.graphics.Rect;
 import android.graphics.drawable.Drawable;
-import android.support.v4.graphics.drawable.DrawableCompat;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.inputmethod.EditorInfo;
@@ -18,8 +18,12 @@ public class NKPasswordEditText extends EditText {
 
     private Drawable showIcon;
     private Drawable hideIcon;
+    /*private Drawable labelIcon;
+    private int labelIconSpace;*/
 
     private int colorAccent;//当前主题的colorAccent
+    //private int labelIconSize;
+    private int passwordIconSize;
 
     private Drawable currentIcon;//当前的显示的图标
 
@@ -41,32 +45,49 @@ public class NKPasswordEditText extends EditText {
         TypedArray typedArray = context.getTheme().obtainStyledAttributes(attrs, R.styleable.NKPasswordEditText, defStyleAttr, 0);
 
         showIcon = typedArray.getDrawable(R.styleable.NKPasswordEditText_showIcon);
-        hideIcon = typedArray.getDrawable(R.styleable.NKPasswordEditText_showIcon);
+        hideIcon = typedArray.getDrawable(R.styleable.NKPasswordEditText_hideIcon);
+        passwordIconSize = (int) typedArray.getDimension(R.styleable.NKPasswordEditText_passwordIconSize,getTextSize());
+
+        /*labelIcon = typedArray.getDrawable(R.styleable.NKPasswordEditText_labelIcon);
+        labelIconSpace = (int) typedArray.getDimension(R.styleable.NKPasswordEditText_labelIconSpace,0f);
+        labelIconSize = (int) typedArray.getDimension(R.styleable.NKPasswordEditText_labelIconSize,getTextSize());*/
+
         typedArray = context.getTheme().obtainStyledAttributes(new int[]{android.R.attr.colorAccent});
         colorAccent = typedArray.getColor(0,0xFF00FF);
         typedArray.recycle();
 
-        //默认图标
-        if (showIcon == null) {
+        if ((showIcon == null && hideIcon != null)||(showIcon != null && hideIcon == null)) {
+            currentIcon = null;
+            throw new IllegalArgumentException("需要同时设置显示图标和隐藏图标");
+        }
+
+        /*//默认图标
+        if (showIcon == null ) {
             showIcon = getResources().getDrawable(R.drawable.wifi_eye_kejian);
         }
         if (hideIcon == null) {
             hideIcon = getResources().getDrawable(R.drawable.wifi_eye);
+        }*/
+        if (showIcon != null && hideIcon != null) {
+            showIcon.setBounds(0, 0, passwordIconSize, passwordIconSize);
+            hideIcon.setBounds(0, 0, passwordIconSize, passwordIconSize);
+            currentIcon = hideIcon;
+            setInputType(EditorInfo.TYPE_CLASS_TEXT | EditorInfo.TYPE_TEXT_VARIATION_PASSWORD);
         }
-
-        showIcon.setBounds(0, 0, (int) getTextSize(), (int) getTextSize());
-        hideIcon.setBounds(0, 0, (int) getTextSize(), (int) getTextSize());
-        DrawableCompat.setTint(showIcon,colorAccent);
-        DrawableCompat.setTint(hideIcon,colorAccent);
-        currentIcon = hideIcon;
-        setInputType(EditorInfo.TYPE_CLASS_TEXT | EditorInfo.TYPE_TEXT_VARIATION_PASSWORD);
+        /*if (labelIcon != null) {
+            labelIcon.setBounds(0, 0, labelIconSize, labelIconSize);
+            setCompoundDrawables(labelIcon,null,null,null);
+            setCompoundDrawablePadding(labelIconSpace);
+        }*/
+        //DrawableCompat.setTint(showIcon,colorAccent);
+        //DrawableCompat.setTint(hideIcon,colorAccent);
     }
 
 
     @Override
     protected void onTextChanged(CharSequence text, int start, int lengthBefore, int lengthAfter) {
         super.onTextChanged(text, start, lengthBefore, lengthAfter);
-        if (text.length() >0) {
+       if (text.length() >0) {
             setCompoundDrawables(null,null,currentIcon,null);
         }else {
             setCompoundDrawables(null,null,null,null);
@@ -112,4 +133,10 @@ public class NKPasswordEditText extends EditText {
             setInputType(EditorInfo.TYPE_CLASS_TEXT | EditorInfo.TYPE_TEXT_VARIATION_PASSWORD);
         }
     }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+    }
+
 }
